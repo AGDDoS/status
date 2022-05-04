9335a886b946099f747e45f51f386d0211e20517
Zhen Chan <79680431+AGDDoS-bot@users.noreply.github.com>
2022-05-04 19:35:01 +0800

    Update zh_cn.js

--- lang/zh_cn.js
+++ lang/zh_cn.js
@@ -3,8 +3,8 @@ _.setTranslation({
     "lang": "语言",
     "No Data Available: Health check was not performed.": "没有可用的数据：健康检查没有部署",
     "No downtime recorded today.": "今天没有Down记录",
-    "Partial outages recorded today.": "",
-    "Major outages recorded today.": "",
+    "Partial outages recorded today.": "今天有部分停运的记录",
+    "Major outages recorded today.": "今天有完全停止运行的记录",
     "Unknown": "未知",
     //getStatusText
     "No Data Available":"没有可用的数据",
