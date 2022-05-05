483dac9c5b34b421281b44f318fc1b10cfbda820
Zhen Chan <79680431+AGDDoS-bot@users.noreply.github.com>
2022-05-05 22:11:33 +0800

    Update zh_cn.js

--- lang/zh_cn.js
+++ lang/zh_cn.js
@@ -1,6 +1,6 @@
 _.setTranslation({
-    "SLA": "See, we always reach our SLA requirement (0%+)!",
-    "lang": "语言",
+    "SLA": "看, 我们经常达到我们的 SLA 要求 (0%+)!",
+    "lang": "语言: 简体中文",
     "No Data Available: Health check was not performed.": "没有可用的数据：健康检查没有部署",
     "No downtime recorded today.": "今天没有Down记录",
     "Partial outages recorded today.": "今天有部分停运的记录",
@@ -8,7 +8,7 @@ _.setTranslation({
     "Unknown": "未知",
     //getStatusText
     "No Data Available":"没有可用的数据",
-    "Fully Operational":"全部运行",
+    "Fully Operational":"所有系统运行",
     "Partial Outage":"部分停运",
-    "Major Outage":"完全炸了",
+    "Major Outage":"停止运行",
   })
